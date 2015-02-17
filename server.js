3a56668e81044fcd4aad51af8b91fe9db5dabd0a
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-02-17 15:41:40 +0000

    sometimes i dream about cheez

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ function generateUUID() {
 function generateStatement(actor,verb,object,context){
     var bones = _.deepClone(statementSkeleton);
     // do things here
+    bones.actor = actor
     return bones;
 }
 
@@ -98,7 +99,7 @@ io.on('connection', function(socket){
         courses[data.course.id].users.push(newUUID);
         // HC SVNT MOCKVS DATVM
         courses[data.course.id].feed[new Date().toISOString()] = "HC SVNT MOCKVS DATVM";
-        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
+//        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
         var uData = {users: users, courses: courses, shop: shop};
         socket.emit('regUser',uData);
     });
