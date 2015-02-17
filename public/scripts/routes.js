93f60b63e08af606905697f425765a483c41d09a
Zaturrby <rjkorteschiel@gmail.com>
2015-02-17 14:46:38 +0100

    add changes to the templates, routes and navigation:

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -9,27 +9,22 @@
     
     $stateProvider
       .state("feed", {
-        url: "/feed",
+        url: "/",
         templateUrl: "../templates/feed.html",
         controller: "FeedCtrl as feed"
       })
       .state("users", {
-        url: "/users",
         templateUrl: "../templates/users.html",
         controller: "UserCtrl as user"
       })
       .state("shop", {
-        url: "/shop",
         templateUrl: "../templates/shop.html",
         controller: "ShopCtrl as shop"
       })
       .state("profile", {
-        url: "/profile",
         templateUrl: "../templates/profile.html",
         controller: "ProfileCtrl as profile"
       });
-
-    $urlRouterProvider.when('/', '/feed');
   }
   
 })();
