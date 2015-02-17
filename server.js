181e9fa7d38564c934f14fcb0b6ef1ef74cb6e1f
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-02-17 15:32:38 +0000

    LOUIS LOUIS GUCCI GUCCI FENDI FENDI PRADA, BASIC BITCHES WEAR THAT SHIT SO I DON'T EVEN BOTHER

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use('/', express.static(__dirname + '/public'));
 var users = {};
 
 var courses = {
-    "wetenschapsfilosofie": {}
+    "wetenschapsfilosofie": {users:[],feed:{}}
 };
 
 var shop = {};
@@ -98,7 +98,7 @@ io.on('connection', function(socket){
         courses[data.course.id].users.push(newUUID);
         // HC SVNT MOCKVS DATVM
         courses[data.course.id].feed[new Date().toISOString()] = "HC SVNT MOCKVS DATVM";
-//        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
+        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
         var uData = {users: users, courses: courses, shop: shop};
         socket.emit('regUser',uData);
     });
