e73cdcd510a86c937f4105e2d71a15b27b24a9d6
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-02-17 12:01:46 +0000

    tincan

--- server.js
+++ server.js
@@ -3,15 +3,22 @@
 var app = require('express')();
 var http = require('require').Server(app);
 var io = require('socket.io')(http);
+var tincan = require('tincanjs');
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.get('/',function(req,res){
     res.sendfile('public/index.html')
 });
 
+var users = {};
+var course = {};
+var shop = {};
+
 io.on('connection', function(socket){
     console.log("SMOKE CRACK AND HAIL XENU");
-    socket.on('userReg',function(data){});
+    socket.on('userReg',function(data){
+        
+    });
     socket.on('userLogin',function(data){});
     socket.on('userPost',function(data){});
     socket.on('userComment',function(data){});
