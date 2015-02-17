b33fbb101c2f118ca16a6e26f9e7c975575eebb1
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-02-17 15:49:14 +0000

    BLOOD FOR THE BLOOD GOD

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ function generateUUID() {
 }
 
 function generateStatement(actor,verb,object,context){
-    var bones = _.deepClone(statementSkeleton);
+//    var bones = _.deepClone(statementSkeleton);
+    var bones = statementSkeleton;
     // do things here
     bones.actor = actor;
     bones.verb = {
@@ -109,7 +110,7 @@ io.on('connection', function(socket){
         courses[data.course.id].users.push(newUUID);
         // HC SVNT MOCKVS DATVM
         courses[data.course.id].feed[new Date().toISOString()] = "HC SVNT MOCKVS DATVM";
-        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
+//        tincan.sendStatement(generateStatement(data.user,"registered",data.course));
         var uData = {users: users, courses: courses, shop: shop};
         socket.emit('regUser',uData);
     });
