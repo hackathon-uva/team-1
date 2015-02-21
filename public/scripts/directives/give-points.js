68dc425fc587aba9b3db79858b33c2dc9b9b9fea
Zaturrby <rjkorteschiel@gmail.com>
2015-02-21 16:10:14 +0100

    directive active, now point giving logic

--- public/scripts/directives/give-points.js
+++ public/scripts/directives/give-points.js
@@ -1,50 +1,66 @@
 // point giving directive
 
-// This was on element
-// onmousemove="distributePoints()" onclick="grantPoints()" onmouseleave="fuckOff()"
+angular.module('kudos').directive('givePoints', function() {
+  return {
+    restrict: 'A',
+    link: function ($scope, element, attrs) {
 
-// All this code was in global
+        element.bind('mousemove', function () {
+            element.html('You moved over me!');
+        });
+        element.bind('click', function () {
+            element.html('you clicked me!');
+        });
+        element.bind('mouseleave', function () {
+            element.html('you left me alone...');
+        });
 
-var availablePoints = 10,
-    givenPoints = 0,
-    grantedPoints = 0;
+        var availablePoints = 10,
+		    givenPoints = 0,
+		    grantedPoints = 0;
 
-var div = document.getElementById("dude1");
+		var div = document.getElementById("dude1");
 
-function distributePoints() {
-  //calculate where the cursor is now relative from the start of the bar
-  var cursor = event.clientX;
-  var right = div.clientWidth;
+		function distributePoints() {
+		  //calculate where the cursor is now relative from the start of the bar
+		  var cursor = event.clientX;
+		  var right = div.clientWidth;
 
-  //calculate percentage of points that will be distributed through givePoints()
-  var points = ((cursor / right) * availablePoints);
-  points = Math.round(points);
+		  //calculate percentage of points that will be distributed through givePoints()
+		  var points = ((cursor / right) * availablePoints);
+		  points = Math.round(points);
 
-  //set given points to a certain value
-  if(points <= availablePoints){
-    givenPoints  = points;
-    div.innerHTML = points;
-  } 
-}
+		  //set given points to a certain value
+		  if(points <= availablePoints){
+		    givenPoints  = points;
+		    div.innerHTML = points;
+		  } 
+		}
 
-function grantPoints() {
-  //send given points to some schmuck
-  if(availablePoints >= givenPoints){
-    grantedPoints += givenPoints;
-    availablePoints -= givenPoints;
-    document.getElementById("points").innerHTML = grantedPoints;
+		function grantPoints() {
+		  //send given points to some schmuck
+		  if(availablePoints >= givenPoints){
+		    grantedPoints += givenPoints;
+		    availablePoints -= givenPoints;
+		    document.getElementById("points").innerHTML = grantedPoints;
+		  }
+		  //set given points to 0
+		  div.innerHTML = "";
+		  givenPoints = 0;
+		}
+
+		function fuckOff() {
+		  //give points back if not given through grantPoints()
+		  if(givenPoints != 0){
+		    availablePoints += givenPoints;
+		  }
+		  //set distributed points to 0
+		  div.innerHTML = "";
+		  givenPoints = 0;
+		}
+
+
+
+        }
   }
-  //set given points to 0
-  div.innerHTML = "";
-  givenPoints = 0;
-}
-
-function fuckOff() {
-  //give points back if not given through givepoints()
-  if(givenPoints != 0){
-    availablePoints += givenPoints;
-  }
-  //set distributed points to 0
-  div.innerHTML = "";
-  givenPoints = 0;
-}
+});
