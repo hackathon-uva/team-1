80846b7612d4e126324641b4e0835805454cde64
Zaturrby <rjkorteschiel@gmail.com>
2015-02-21 14:49:30 +0100

    moved data to router, and added new, plus some transitional styling

--- public/scripts/controllers/feedCtrl.js
+++ public/scripts/controllers/feedCtrl.js
@@ -1,39 +1,14 @@
 (function(){
   'use strict';
 
-  angular.module('kudos').controller('FeedCtrl', [FeedCtrl]);
+  angular.module('kudos').controller('FeedCtrl', ['course', FeedCtrl]);
 
-	function FeedCtrl(){
+	function FeedCtrl(course){
 	  	var vm = this;
 
-	  	var course = { 
-	  		name: 'Wetenschapsfilosofie',
-	  		teacher: 'Jaques Bos',
-	  		feed: [
-	  			{
-	  				title: 		'The first post',
-	  				author: 	'Karel de Grote',
-	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
-	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
-	  			},
-	  			{
-	  				title: 		'The second post',
-	  				author: 	'Karel de Grote',
-	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
-	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
-	  			},
-	  			{
-	  				title: 		'The third post',
-	  				author: 	'Karel de Grote',
-	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
-	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
-	  			}
-	  		]
-	  	};
+
 
 	  	vm.course = course; 
-
-
 
 	  	// var socket = io();
 	  	// var userData = {
--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -6,6 +6,49 @@
   function routes($stateProvider, $locationProvider, $urlRouterProvider) {
 
    // $locationProvider.html5Mode(true);
+
+    var courseData = { 
+        name: 'Wetenschapsfilosofie',
+        teacher: 'Jaques Bos',
+        students: [ 
+          { 
+            name: 'Brad Pitt',
+            points: 50,
+          },
+          { 
+            name: 'Angela Jolie',
+            points: 100,
+          },
+          { 
+            name: 'Hugh Hefner',
+            points: 0,
+          },
+          { 
+            name: 'Harry Potter',
+            points: 350,
+          }
+        ],
+        feed: [
+          {
+            title:    'The first post',
+            author:   'Karel de Grote',
+            contents:   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+            date:     'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
+          },
+          {
+            title:    'The second post',
+            author:   'Karel de Grote',
+            contents:   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+            date:     'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
+          },
+          {
+            title:    'The third post',
+            author:   'Karel de Grote',
+            contents:   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+            date:     'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
+          }
+        ]
+    };
     
     $stateProvider
       .state("login", {
@@ -16,7 +59,12 @@
       .state("feed", {
         url: "/feed",
         templateUrl: "../templates/feed.html",
-        controller: "FeedCtrl as feed"
+        controller: "FeedCtrl as feed",
+        resolve: {
+          course: function(){
+            return courseData;
+          }
+        }
       })
       .state("users", {
         url: "/users",
