1c5da813f9275e00b22aa66641acaa91402da6bd
Zaturrby <rjkorteschiel@gmail.com>
2015-02-21 13:08:15 +0100

    feed templated, some styling

--- public/scripts/controllers/feedCtrl.js
+++ public/scripts/controllers/feedCtrl.js
@@ -6,33 +6,62 @@
 	function FeedCtrl(){
 	  	var vm = this;
 
-	  	var socket = io();
-	  	var userData = {
-	  		user: {
-	  			account: {
-	  				name: 'Klaas Tester',
-	  				homePage: 'url'
+	  	var course = { 
+	  		name: 'wetenschapsfilosofie',
+	  		teacher: 'Jaques Bos',
+	  		feed: [
+	  			{
+	  				title: 		'The first post',
+	  				author: 	'Karel de Grote',
+	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
+	  			},
+	  			{
+	  				title: 		'The second post',
+	  				author: 	'Karel de Grote',
+	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
+	  			},
+	  			{
+	  				title: 		'The third post',
+	  				author: 	'Karel de Grote',
+	  				contents: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio aut voluptatem velit ex nesciunt enim explicabo expedita recusandae, harum accusantium facilis optio rem nulla sed quis nihil, eius totam necessitatibus.',
+	  				date: 		'Sat Feb 21 2015 12:41:01 GMT+0100 (CET)'
 	  			}
-	  		},
-	  		course: {
-	  			objectType: 'Activity',
-	  			id: 'wetenschapsfilosofie'
-	  		}
+	  		]
 	  	};
-	  	socket.emit('userReg', userData);
-	  	socket.on('regUser', function(data){
-	  		console.log('all data received');
-	  		console.log(data);
-	  		vm = data;
-	  		socket.emit('userPost', {
-	  			payload: userData,
-	  			message: "Some very cool message!"
-	  		});
-	  	});
-	  	socket.on('updFeed', function(data){
-	  		console.log('feedupdate required');
-	  		console.log(data);
-	  	});
+
+	  	vm.course = course; 
+
+
+
+	  	// var socket = io();
+	  	// var userData = {
+	  	// 	user: {
+	  	// 		account: {
+	  	// 			name: 'Klaas Tester',
+	  	// 			homePage: 'url'
+	  	// 		}
+	  	// 	},
+	  	// 	course: {
+	  	// 		objectType: 'Activity',
+	  	// 		id: 'wetenschapsfilosofie'
+	  	// 	}
+	  	// };
+	  	// socket.emit('userReg', userData);
+	  	// socket.on('regUser', function(data){
+	  	// 	console.log('all data received');
+	  	// 	console.log(data);
+	  	// 	vm = data;
+	  	// 	socket.emit('userPost', {
+	  	// 		payload: userData,
+	  	// 		message: "Some very cool message!"
+	  	// 	});
+	  	// });
+	  	// socket.on('updFeed', function(data){
+	  	// 	console.log('feedupdate required');
+	  	// 	console.log(data);
+	  	// });
 	  	
 		return vm;
 	}
