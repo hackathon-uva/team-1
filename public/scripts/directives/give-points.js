f09a12bbf9263a82b6c42fbe5a69431a11fbf568
Zaturrby <rjkorteschiel@gmail.com>
2015-02-21 18:55:26 +0100

    still hacky, but givePointsdirective works

--- public/scripts/directives/give-points.js
+++ public/scripts/directives/give-points.js
@@ -10,7 +10,7 @@ angular.module('kudos').directive('givePoints', function() {
 
 	        element.bind('mousemove', distributePoints);
 	        element.bind('click', grantPoints);
-	        element.bind('mouseleave', done);
+	        element.bind('mouseleave', mouseOut);
 
 	        var availablePoints = 100,
 			    givenPoints = 0,
@@ -22,21 +22,23 @@ angular.module('kudos').directive('givePoints', function() {
 	    		// Im doing something hacky here. Because the element that the event runs on has childeren
 	    		// the values get severly disturbed. So I hardcoded the offset. 
 
-	    		console.log(event.x);
-	    		console.log(element[0].clientWidth);
+	    		console.log('event' + (event.x - 65));
+	    		console.log('element' + element[0].clientWidth);
 
 				var cursor = event.x - 65;
-				var right = element[0].clientWidth;
+				var elementWidth = element[0].clientWidth;
 
 				// Calculate percentage of points that will be distributed through givePoints()
-				var points = ((cursor / right) * availablePoints);
+				var points = 0;
+				points = ((cursor / elementWidth) * 100);
 				points = Math.round(points);
 
+				console.log('points' + points);
 				// Set given points to a certain value
 				if(points <= availablePoints){
 				  	// console.log('statement evaluates true');
 				    givenPoints = points;
-				    element.children()[3].style.background = 'linear-gradient(90deg, #BC0031 '+ (points) +'%, rgb(208, 208, 208) '+ ((points) + (0.1*points))+'%)';
+				    element.children()[3].style.background = 'linear-gradient(90deg, #BC0031 '+ (points - 1) +'%, rgb(208, 208, 208) '+ (points + 1)+'%)';
 				} 
 			}
 
@@ -51,22 +53,14 @@ angular.module('kudos').directive('givePoints', function() {
 				}
 
 				// Set given points to 0
-				// element.children()[3].style.background = 'linear-gradient(90deg, #BC0031 '+ (points) +'%, rgb(208, 208, 208) '+ ((points) + (0.1*points))+'%)';
+				element.children()[3].style.background = 'rgb(208, 208, 208)';
 				givenPoints = 0;
 			}
 
-			function done(event) {
-				// console.log('done points triggered');
-			  	// Give points back if not given through grantPoints()
-			  	
-			  	// if(givenPoints != 0){
-			   //  	availablePoints += givenPoints;
-			  	// }
-
+			function mouseOut(event) {
 			  	// Set distributed points to 0
-
-			  	// div.innerHTML = "";
-			  	// givenPoints = 0;
+			  	element.children()[3].style.background = 'rgb(208, 208, 208)';
+				givenPoints = 0;
 			}
         }
 	}
